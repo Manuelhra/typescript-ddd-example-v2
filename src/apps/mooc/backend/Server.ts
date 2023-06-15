2900f9ce1578a421185f80b59cbc6b35359041d4
manuelh_ra <manuelhra.dev@gmail.com>
2023-06-15 16:59:07 -0500

    Create MoocBackendApp

--- src/apps/mooc/backend/Server.ts
+++ src/apps/mooc/backend/Server.ts
@@ -1,9 +1,10 @@
-import express, { Express, Router } from 'express'
+import express, { Express, NextFunction, Request, Response, Router } from 'express'
 import * as http from 'http'
 import { json, urlencoded } from 'body-parser'
 import helmet from 'helmet';
 import compression from 'compression';
 import errorHandler from 'errorhandler';
+import httpStatus from 'http-status';
 
 import { registerRoutes  } from './routes';
 
@@ -26,7 +27,41 @@ export class Server {
 
         const router: Router = Router();
         router.use(errorHandler);
+        this.express.use(router);
 
-        // Registrar rutas
+        registerRoutes(router);
+
+		router.use((err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+			console.log(err);
+			res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+		});
     } 
+
+    async listen(): Promise<void> {
+        return new Promise((resolve, _reject) => {
+            const env = this.express.get('env') as string;
+            this.httpServer = this.express.listen(this.port, () => {
+                console.log(`Mock Backend App is running at http://localhost:${this.port} in ${env} mode`)
+                console.log('Press CTRL-C to stop');
+                resolve();
+            })
+        })
+    }
+
+    getHTTPServer(): Server['httpServer'] {
+        return this.httpServer;
+    }
+
+    async stop(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.httpServer) {
+                this.httpServer.close((error) => {
+                    if (error) reject(error)
+                    else resolve();
+                })
+            }
+
+            resolve();
+        })
+    }
 }
