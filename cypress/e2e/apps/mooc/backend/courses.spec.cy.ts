a955ccea2823e72023d1e2c72b9136d75842e1e5
manuelh_ra <manuelhra.dev@gmail.com>
2023-06-18 02:31:58 -0500

    Create coursePutController

--- cypress/e2e/apps/mooc/backend/courses.spec.cy.ts
+++ cypress/e2e/apps/mooc/backend/courses.spec.cy.ts
@@ -1,5 +1,17 @@
+import httpStatus from "http-status"
+
 describe('Invoke service Courses', () => {
     it('Create course and response status code should be 201 and the response should be empty', () => {
-        
+        cy.request({
+        method: 'PUT',
+        url: '/courses/ef8ac118-8d7f-49cc-abec-78e0d05af80a',
+        body: { name: 'The best course', duration: "5 hours" },
+        failOnStatusCode: false,
+        })
+        .its('status')
+        .should('equal', httpStatus.CREATED)
+        .end()
+        .its('body')
+        .should('equal', {});
     })
 })
--- src/apps/mooc/backend/dependency-injection/container.ts
+++ src/apps/mooc/backend/dependency-injection/container.ts
@@ -1,11 +1,19 @@
 import * as awilix from 'awilix';
 
 import { StatusGetController } from '../controllers/StatusGetController';
+import { CoursePutController } from '../controllers/CoursePutController';
 
-const container = awilix.createContainer<{ statusGetController: StatusGetController }>({ injectionMode: awilix.InjectionMode.PROXY });
+interface Container {
+    statusGetController: StatusGetController;
+    coursePutController: CoursePutController;
+}
+
+const container = awilix.createContainer<Container>({ injectionMode: awilix.InjectionMode.PROXY });
 
 container.register({
     statusGetController: awilix.asClass(StatusGetController),
+    coursePutController: awilix.asClass(CoursePutController),
 })
 
 export const statusGetController = container.resolve('statusGetController');
+export const coursePutController = container.resolve('coursePutController');
