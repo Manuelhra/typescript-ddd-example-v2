ddf924cafac3863fb3886daea77b960bcf97d6bd
manuelh_ra <manuelhra.dev@gmail.com>
2023-06-17 22:45:02 -0500

    COnfig baseUrl cypress

--- cypress/e2e/apps/mooc/backend/status.spec.cy.ts
+++ cypress/e2e/apps/mooc/backend/status.spec.cy.ts
@@ -1,8 +1,9 @@
 describe('Status server',() => {
   it('Get 200 on request /status', () => {
-    cy.request('GET', 'http://localhost:5000/status')
+    cy.request('GET', '/status')
     .then((response) => {
       expect(response.status).to.equal(200);
+      assert.equal(response.status, 400, 'Match');
     });
   })
 })
