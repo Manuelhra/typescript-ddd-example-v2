6a126965e43f3f2d16c51e57b58505b4f56cc02f
manuelh_ra <manuelhra.dev@gmail.com>
2023-06-19 01:09:06 -0500

    Create first suit with jest

---
+++ src/contexts/mooc/courses/application/CourseCreator.ts
@@ -0,0 +1,14 @@
+import { Course } from "../domain/Course";
+import { CourseRepository } from "../domain/CourseRepository";
+
+export class CourseCreator {
+    constructor(
+        private readonly repository: CourseRepository,
+    ) {}
+
+    async run(id: string, name: string, duration: string): Promise<void> {
+        const course = new Course({ id, name, duration });
+
+        return this.repository.save(course);
+    }
+}
--- test/context/mooc/courses/application/CourseCreator.test.ts
+++ test/context/mooc/courses/application/CourseCreator.test.ts
@@ -1,17 +1,27 @@
+import {
+    describe,
+    expect,
+    test,
+} from '@jest/globals'
+
+import { CourseCreator } from "../../../../../src/contexts/mooc/courses/application/CourseCreator";
+import { Course } from "../../../../../src/contexts/mooc/courses/domain/Course";
+import { CourseRepository } from "../../../../../src/contexts/mooc/courses/domain/CourseRepository";
+
 describe('CourseCreator', () => {
-    it('Should create a valid course', async () => {
-        const repository = {
+    test('Should create a valid course', async () => {
+        const repository: CourseRepository = {
             save: jest.fn(),
         };
 
-        const creator = new CourseCreator();
+        const creator = new CourseCreator(repository);
         const id = 'some-id';
         const name = 'some-name';
         const duration = 'some-duration';
-        const expectedCourse = new Course(id, name, duration);
+        const expectedCourse = new Course({ id, name, duration });
 
         await creator.run(id, name, duration);
         
-        expect(repository.save).to.have.been.called(expectedCourse);
-    })
+        expect(repository.save).toHaveBeenCalledWith(expectedCourse);
+    });
 })
